3c04d2202261a64f48ea67217b6eef962a8a584e
Noemi Santamaria <noemi.santamaria.dev@gmail.com>
2021-09-26 18:51:59 +0200

    Setup project
    Created modules, added H2 database configuration and profiles management

---
+++ module-app/src/main/java/com/springboot/multimodule/SpringbootMultimoduleApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.multimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMultimoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMultimoduleApplication.class, args);
+	}
+
+}
