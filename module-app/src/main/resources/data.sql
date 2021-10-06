5ea6f1b3e51807a13fb6f16d9e8ff2de133cfe08
Noemi Santamaria <noemi.santamaria.dev@gmail.com>
2021-10-06 13:55:44 +0200

    Added postgres dependency and edit properties file
    From H2 to PostgreSQL database

--- module-app/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO SUPERHERO (name, description, thumbnail) VALUES
-('Thor', 'As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he is quite smart and compassionate.  He is self-assured, and he would never, ever stop fighting for a worthwhile cause.', 'http://i.annihil.us/u/prod/marvel/i/mg/d/d0/5269657a74350.jpg'),
-('Loki', '', 'http://i.annihil.us/u/prod/marvel/i/mg/d/90/526547f509313.jpg'),
-('Iron Man', 'Wounded, captured and forced to build a weapon by his enemies, billionaire industrialist Tony Stark instead created an advanced suit of armor to save his life and escape captivity. Now with a new outlook on life, Tony uses his money and intelligence to make the world a safer, better place as Iron Man.', 'http://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55.jpg');
